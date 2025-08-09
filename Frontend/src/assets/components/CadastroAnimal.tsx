319f86af0b19ef978ba68b705aa15b5e58c7fd3f
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-08-09 16:50:58 -0300

    refactor: criando formulario para cadastro de animais

--- Frontend/src/assets/components/CadastroAnimal.tsx
+++ Frontend/src/assets/components/CadastroAnimal.tsx
@@ -20,7 +20,7 @@ const CadastroAnimal = () => {
         habitatId: number;
     }
 
-    //estado controlado do formulário
+    //estado tipado do formulário
     //[acessar, alterar]
     const [form, setForm] = useState<AnimalForm>({
         nome: '',
@@ -31,9 +31,55 @@ const CadastroAnimal = () => {
     });
 
     return (
-        <>
+        //formulario para cadastrar um animal
+        <form>
+            <div className="form-group">
+                <label htmlFor="nome">Nome:</label>
+                <input
+                    type="text"
+                    id="nome"
+                    value={form.nome}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="categoria">Categoria:</label>
+                <input
+                    type="text"
+                    id="categoria"
+                    value={form.categoria}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="dieta">Dieta:</label>
+                <input
+                    type="text"
+                    id="dieta"
+                    value={form.dieta}
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="presenca">Presença:</label>
+                <select
+                    id="presenca"
+                    value={form.presenca}
+                >
+                    <option value="PRESENTE">Presente</option>
+                    <option value="AUSENTE">Ausente</option>
+                </select>
+            </div>
+            <div className="form-group">
+                <label htmlFor="habitatId">Habitat:</label>
+                <select
+                    id="habitatId"
+                    value={form.habitatId}
+                >
+                    <option value="1">Floresta</option>
+                    <option value="2">Savana</option>
+                    <option value="3">Deserto</option>
+                </select>
 
-        </>
+            </div>
+        </form>
     )
 }
 
